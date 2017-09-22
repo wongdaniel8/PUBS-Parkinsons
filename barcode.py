0e57e4a5235ae0bed786a5d10effe2f115e9d1ee
UCSF Student <student@Wenyus-iPad.ucsf.edu>
2017-09-22 16:10:28 -0700

    heatmap

--- barcode.py
+++ barcode.py
@@ -1,4 +1,7 @@
 import cPickle as pic
+import matplotlib.pyplot as plt
+import numpy as np
+
 data = pic.load(open("allele_dic.pkl", "rb"))
 translate_data = pic.load(open("translate.pkl", "rb"))
 aminoNum = pic.load(open("aminotonumber.pkl", "rb"))
@@ -12,10 +15,20 @@ for key in data.keys():
 	aminoAcid = translate_data[complement]
 	num = aminoNum[aminoAcid]
 	ogDict[key] = [residue, codonMut, aminoAcid, num]
-print(ogDict)
+# print(ogDict)
 
 
+x = [1,1,1,2,2,2]
+y = [1,1,1,2,2,2]
+heatmap, xedges, yedges = np.histogram2d(x, y)
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+plt.clf()
+plt.imshow(heatmap.T, extent=extent, origin='lower')
+plt.show()
 
+
+# plt.imshow(a, cmap='hot', interpolation='nearest')
+# plt.show()
 #NOTES:
 # (barcode: aa residue mutated _ resulting in codon)
 # 150 aa protein
